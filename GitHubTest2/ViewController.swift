db385502041408eb2e253b608ecab741564ef8fc
Nick Irving <nickirving@nicholass-macbook-pro.local>
2015-12-08 23:40:54 +0000

    Initial Commit

---
+++ GitHubTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest2
+//
+//  Created by Nick Irving on 08/12/2015.
+//  Copyright © 2015 Happy Shark Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
