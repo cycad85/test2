5b08ca65cf177593056118b0d3c13c57e8962e9e
Nick Irving <nickirving@nicholass-macbook-pro.local>
2015-12-08 23:56:22 +0000

    Initial commit

---
+++ myViewController.swift
@@ -0,0 +1,41 @@
+//
+//  myViewController.swift
+//  GitHubTest2
+//
+//  Created by Nick Irving on 08/12/2015.
+//  Copyright © 2015 Happy Shark Software. All rights reserved.
+//
+
+import UIKit
+
+class myViewController: UIViewController {
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello GitHub!")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
